d48b5dd816014a2f6ecbab46c080236cd28a7127
John W. Leimgruber III <leimgrub@gmail.com>
2024-04-28 17:38:49 -0400

    add v0.1 mod configs
    Basically default configs except for a few things:
    
    * remove player name in multiplayer mod config (defaults to steam name)
    * assign multiplayer mod ping mouse buttons
    * assign minimap ping mouse buttons
    * drop master volume and music volume in Prefs
    * mark RimHUD tutorial complete

--- pin-mods.sh
+++ pin-mods.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # only pin from workshop the first time
-# 7z a rwmpksr-vTODO.zip ~/.steam/steam/steamapps/workshop/content/294100/*
+# 7z a multinomaly-vTODO.zip ~/.steam/steam/steamapps/workshop/content/294100/*
 ## Windows Locations Local Copy of Mods
 # /c/Program Files (x86)/Steam/steamapps/common/RimWorld/Mods/3132099594
 ## Windows Locations Steam Workshop Copy of Mods
 # /c/Program Files (x86)/Steam/steamapps/workshop/content/294100/3132099594
 # henceforth keep the original versions
-7z a rwmpksr-vTODO.zip ~/.steam/steam/steamapps/common/RimWorld/Mods/*
+7z a multinomaly-vTODO.zip ~/.steam/steam/steamapps/common/RimWorld/Mods/*
--- update.sh
+++ update.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 #Linux
 rsync -avh /home/garm/.config/unity3d/Ludeon\ Studios/RimWorld\ by\ Ludeon\ Studios/Config ./
-rsync -avh /home/garm/.config/unity3d/Ludeon\ Studios/RimWorld\ by\ Ludeon\ Studios/HugsLib ./
+#rsync -avh /home/garm/.config/unity3d/Ludeon\ Studios/RimWorld\ by\ Ludeon\ Studios/HugsLib ./
 #Windows
 #rsync -avh /mnt/win11/Users/ubergarm/AppData/LocalLow/Ludeon\ Studios/RimWorld\ by\ Ludeon\ Studios/Config ./
 #rsync -avh /mnt/win11/Users/ubergarm/AppData/LocalLow/Ludeon\ Studios/RimWorld\ by\ Ludeon\ Studios/HugsLib ./
-git checkout Config/Knowledge.xml
-git checkout Config/Mod_1662119905_DubsMintMinimapMod.xml
-git checkout Config/Mod_2606448745_Multiplayer.xml
-git checkout Config/Mod_1235181370_DesignatorShapes.xml
-git checkout Config/Mod_1579516669_DubsPaintShopMod.xml
+#git checkout Config/Knowledge.xml
+#git checkout Config/Mod_1662119905_DubsMintMinimapMod.xml
+#git checkout Config/Mod_2606448745_Multiplayer.xml
